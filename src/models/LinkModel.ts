186a0094c08fe6e31694e8e686b36cea632a67a4
viktorcus <carolynseglem@gmail.com>
2023-03-23 21:02:44 -0500

    Added code to create link ids.

--- src/models/LinkModel.ts
+++ src/models/LinkModel.ts
@@ -1,3 +1,4 @@
+import { createHash } from 'crypto';
 import { AppDataSource } from '../dataSource';
 import { Link } from '../entities/Link';
 
@@ -11,4 +12,13 @@ async function getLinkById(userId: string): Promise<Link | null> {
     .getOne();
 }
 
-export { getLinkById };
+function createLinkId(originalUrl: string, userId: string): string {
+  const md5 = createHash('md5');
+  md5.update(originalUrl + userId);
+  const urlHash = md5.digest('base64url');
+  const linkId = urlHash.slice(9);
+
+  return linkId;
+}
+
+export { getLinkById, createLinkId };
